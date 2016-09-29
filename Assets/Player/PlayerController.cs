9ac42ff81cb5753a6056c9daa62f50dd215d9721
Jehutyver10 <juan.irizarry.cole@gmail.com>
2016-09-29 00:20:29 -0400

    Cleared up some camera glitches

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -58,10 +58,12 @@ public class PlayerController: MonoBehaviour {
 					AdjustPseudo();
 				}
 			}else{
-				pseudo.transform.rotation = transform.rotation;
+					pseudo.transform.rotation = transform.rotation;
 			}
 		}else{
-				AdjustPseudo();
+			if(isLocked){
+				//AdjustPseudo();
+			}
 		}
 
 	}
