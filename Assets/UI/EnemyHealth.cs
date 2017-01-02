2d515d911951163397f0a71a079c1bc54ae149c9
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2017-01-02 14:04:45 -0500

    Adjusted the health ui objects

---
+++ Assets/UI/EnemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : HealthText {
+	PlayerController player;
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindObjectOfType<PlayerController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.target) {
+			GetComponent<Text>().text = associatedObjectName + ": " + player.target.GetComponent<Health>().health;
+		} else {
+			GetComponent<Text> ().text = null;
+		}
+	}
+}
